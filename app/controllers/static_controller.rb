950ff6465109e9f2802daa47bc3c8effbc51d8a5
github username <github email address>
2019-12-14 22:38:53 -0600

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+
+def about
+
+    #render "some_page" #folder route 'static/some_page' not reqiured, it looks for the same name in the views folder all files
+
+end
+
+end
